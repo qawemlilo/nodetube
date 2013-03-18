c9e0c41d3984514f6355d5d44d7fbd2ebe5a4c80
Raging Flame <qawemlilo@gmail.com>
2013-03-18 20:59:50 +0200

    ...

--- download.js
+++ download.js
@@ -4,24 +4,7 @@ var ytdl = require('ytdl'),
 
 
 function Download (url, options) {
-    var readStream = ytdl(url, options),
-        dataRead = 0,
-        progress = 0,
-        self = this; 
-        
-    self.progress = 0;
-   
-    readStream.on('info', function (info, format) {
-        var bar = new ProgressBar('  downloading [:bar] :percent :etas', {complete: '=', incomplete: ' ', width: 40, total: parseInt(format.size, 10)});
-        
-        readStream.on('data', function (data) {
-            bar.tick(data.length);
-        });
-        
-        readStream.on('end', function(){
-            console.log('\n');
-        });
-    });
+    var readStream = ytdl(url, options);
     
     return readStream;
 }
