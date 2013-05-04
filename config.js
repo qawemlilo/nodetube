47241e2c7b2818989009379a1947ab705b341645
Raging Flame <qawemlilo@gmail.com>
2013-05-04 14:12:38 +0200

    ...

--- config.js
+++ config.js
@@ -1,11 +1,17 @@
 
 var folder, videos;
 
-folder = '../../../youtube-downloads'; // e.g ../downloads or ./downloads
+folder = '../youtube-downloads'; // e.g ../downloads or ./downloads
 
 videos = [
-    'https://www.youtube.com/watch?v=T2BX-LZOYsY',
-    'https://www.youtube.com/watch?v=T2BXjghhjsY'
+    'http://www.youtube.com/watch?v=zva_GETXimI',
+    'http://www.youtube.com/watch?v=PzGC4ub6RDg',
+    'http://www.youtube.com/watch?v=QSt9rBa_oUM',
+    'http://www.youtube.com/watch?v=AjQ5e9TOZVk',
+    'http://www.youtube.com/watch?v=tX8DC9q6rv0',
+    'http://www.youtube.com/watch?v=WeB1knLoLYk',
+    'http://www.youtube.com/watch?v=USk1ie30z5k',
+    'http://www.youtube.com/watch?v=84j61_aI0q8'
 ];
 
 module.exports.folder = folder;
